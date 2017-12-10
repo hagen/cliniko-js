1bbe6816af95d125a5876bf1edfe965a4d470fe4
Hagen Dittmer <hagen.dittmer@outlook.com>
2017-12-10 19:04:22 +1100

    Changed structure a bit. Wasn't working in Lambda.

--- index.js
+++ index.js
@@ -2,149 +2,174 @@
 // Apparently this library can resolve it. However, you have
 // to monkey patch the http_parser for it to work, so require it
 // before request.
-process.binding('http_parser').HTTPParser = require('http-parser-js').HTTPParser;
+process.binding('http_parser').HTTPParser = require('http-parser-js').HTTPParser
 const request = require('request')
-const retry = require('promise-retry')
-const RETRY_OPTS = { retries : 2, factor : 2, randomize : true }
+const jitter = require('promise-retry')
+const DEFAULT_RETRY_OPTS = {
+  retries: 2,
+  factor: 2,
+  randomize: true
+}
 const CLINIKO_API_BASE = "https://api.cliniko.com/v1"
+const OPERATORS = [
+  { std: '<=', mod : ':<=' },
+  { std: '>=', mod : ':>=' },
+  { std: '~~', mod : ':~~' },
+  { std: '!=', mod : ':!=' },
+  { std: '<>', mod : ':!=' },
+  { std: '<', mod : ':<' },
+  { std: '>', mod : ':>' },
+  { std: '=', mod : ':=' },
+  { std: '~', mod : ':~' },
+  { std: '', mod : '' }
+]
 
 function NoAPIKeyError(message) {
-  this.name = "NoAPIKeyError";
-  this.message = message;
+  this.name = "NoAPIKeyError"
+  this.message = message
 }
-NoAPIKeyError.prototype = new Error();
+NoAPIKeyError.prototype = new Error()
 
 function NoUserAgentError(message) {
-  this.name = "NoUserAgentError";
-  this.message = message;
+  this.name = "NoUserAgentError"
+  this.message = message
 }
-NoUserAgentError.prototype = new Error();
+NoUserAgentError.prototype = new Error()
+
+function TooManySearchParametersError(message) {
+  this.name = "TooManySearchParametersError"
+  this.message = message
+}
+TooManySearchParametersError.prototype = new Error()
 
 /**
  * Get the Cliniko settings data
  * @param  {[type]} api_key    [description]
  * @return {[type]}            [description]
  */
-function prepareUrl( target, query_string ) {
-  return CLINIKO_API_BASE + target + ( query_string ? query_string : "")
+const appendSearch = ( uri, search ) => {
+  const qs = search.reduce((acc, entry) => {
+    let query = OPERATORS.reduce((str, op) => {
+      if (entry.includes(op.standard) && !entry.includes(op.modified)) {
+        return entry.replace(op.standard, op.modified)
+      }
+      return str
+    }, "")
+    acc.push(query)
+  }, []).join("&")
+  return [uri, qs].join("")
 }
 /**
- * Queries Cliniko API with supplied parameters.
- * This function is called by a runner implementing jitter. *
- * @param  {[type]} url        [description]
- * @param  {[type]} body       [description]
- * @param  {[type]} method     [description]
- * @param  {[type]} api_key    [description]
- * @param  {[type]} user_agent [description]
- * @return {[type]}            [description]
+ * Create a path with the id
  */
-function safeQuery(url, body, method, api_key, user_agent) {
-  return new Promise( ( resolve, reject ) => {
-    const options = {
-      method: method,
-      url: url,
-      auth: {
-        username : api_key,
-        password : "",
-        sendImmediately : true
-      },
-      headers: {
-        'User-Agent': user_agent,
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      json: body || true
-    }
-    // Run the HTTP request
-    request(options, (err, response, json) => {
-      if (err) reject(err)
-      else {
-        // Resolve withe JSON response, or with an empty object
-        resolve( !json || json === null ? {} : json)
-      }
-    })
-  })
+const appendId = ( uri, id ) => {
+  return [uri, "/", id].join("")
 }
-const query = ({ api_key, body, method, path, query_string, user_agent }) => {
-  return new Promise((resolve, reject) => {
-    // Must have an API key
-    if (!api_key || api_key.length === 0) {
-      return reject(new NoAPIKeySuppliedError(`No API key was supplied for the target ${target}, user agent ${user_agent}`))
-    }
-    // Build the URL and submit
-    const url = prepareUrl(path, query_string)
-    // Submit with Jitter
-    retry(RETRY_OPTS, (retry, number) => {
-      console.log(`[STATUS] Attempt ${number} running HTTPs ${method} to Cliniko (${user_agent}):`, url)
-      return safeQuery(url, body, method, api_key, user_agent)
-        .then(resolve)
-        .catch(retry)
-    }).catch(reject)
-  })
-}
-function APIPublicSettings(cliniko) {
-  const path = "/settings/public"
-  return {
-    get : function() {
-      return cliniko.get({ path })
-    }.bind(this)
-  }
-}
-function APISettings(cliniko) {
-  const path = "/settings"
-  return {
-    Public: function() {
-      return APIPublicSettings(cliniko)
-    }.bind(this),
-    get: function() {
-      return cliniko.get({ path })
-    }.bind(this)
-  }
-}
-function APIUser(cliniko) {
-  const path = "/user"
-  return {
-    get: function() {
-      return cliniko.get({ path })
-    }.bind(this)
-  }
-}
-function APIUsers(cliniko, id) {
-  const path = "/users"
-  return {
-    get: function() {
-      return cliniko.get({ path }, id)
-    }.bind(this)
-  }
-}
-function Cliniko({ api_key, user_agent }) {
+const Cliniko = exports.Cliniko = function({ api_key, user_agent, retries }) {
+  this.retries = retries || DEFAULT_RETRY_OPTS.retries
+
   if(!api_key) throw new NoAPIKeyError()
   if(!user_agent) throw new NoUserAgentError()
-  this.api_key = api_key
-  this.user_agent = user_agent
-  this.Settings = function() {
-    return APISettings(this)
-  }.bind(this)
-  this.User = function() {
-    return APIUser(this)
-  }.bind(this)
-  this.Users = function(id) {
-    return APIUsers(this, id)
-  }.bind(this)
-}
-Cliniko.prototype.get = function(entity, id) {
-  return new Promise(function(resolve, reject) {
-    const opts = {
-      api_key: this.api_key,
-      body: null,
-      method: "get",
-      path : entity.path + ( id ? `/${id}` : "" ),
-      query_string: null,
-      user_agent : this.user_agent,
+  this.api_key = api_key.trim()
+  this.user_agent = user_agent.trim()
+  /**
+   * Request
+   */
+  this._request = function({ method, uri, body }) {
+    return new Promise(function( resolve, reject ) {
+      const options = {
+        method,
+        url : CLINIKO_API_BASE + uri,
+        auth: {
+          username: this.api_key,
+          password: "",
+          sendImmediately: true
+        },
+        headers: {
+          'User-Agent': this.user_agent,
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        json: body || true
+      }
+      // Run the HTTP request
+      request(options, function(err, response, json) {
+        if (err) reject(err)
+        else {
+          // Resolve withe JSON response, or with an empty object
+          resolve( !json || json === null ? {} : json)
+        }
+      })
+    }.bind(this))
+  }
+  /**
+   * Request wrapper with Jitter retries
+   */
+  this._doRequest = function({ method, uri, body }) {
+    return new Promise(function(resolve, reject) {
+      // Submit with Jitter retry
+      jitter(Object.assign({}, DEFAULT_RETRY_OPTS, {
+        retires : this.retries
+      }), function(retry, number) {
+        return this._request({ method, uri, body })
+        .catch(retry)
+      }.bind(this))
+      .then(resolve, reject)
+    }.bind(this))
+  }
+  /**
+   * GET
+   */
+  this._get = function({ path, id, search }) {
+    const method = "get"
+    // Can't have both ID and search
+    if (id && search) {
+      throw new TooManySearchParametersError(`When issuing a GET on ${path}, you can supply either the entity ID (id), or search parameters (query string).`)
     }
-    query(opts, id)
-      .then(resolve)
-      .catch(reject)
-  }.bind(this))
-}
-module.exports = Cliniko
+    let uri = path
+    if (search) uri = appendSearch(uri += "?", search)
+    else if (id) uri = appendId(uri, id) + "?"
+    else uri += "?"
+    return this._doRequest({ method, uri })
+  }
+  /**
+   * PUT
+   */
+  this._put = function({ path, id, body }) {
+    const method = "put"
+    return this._doRequest({ method, path, id, body })
+  }
+  /**
+   * POST
+   */
+  this._post = function({ path, body }) {
+    const method = "post"
+    return this._doRequest({ method, path, body })
+  }
+  /**
+   * DELETE
+   */
+  this._delete = function({ path, body }) {
+    const method = "delete"
+    return this._doRequest({ method, path, id })
+  }
+};
+
+(function() {
+  this.getSettings = function() {
+    const path = "/settings"
+    return this._get({ path })
+  }
+  this.getPublicSettings = function() {
+    const path = "/settings/public"
+    return this._get({ path })
+  }
+  this.getUser = function() {
+    const path = "/user"
+    return this._get({ path })
+  }
+  this.getUsers = function({id, search }) {
+    const path = "/users"
+    return this._get({ path, id, search })
+  }
+}).call(Cliniko.prototype)
--- test.js
+++ test.js
@@ -1,14 +1,12 @@
-const Cliniko = require('./index')
+const Cliniko = require('./').Cliniko
 const opts = {
-  api_key: "",
-  user_agent: "Test user-agent"
+  api_key: "<YOUR API KEY>",
+  user_agent: "<YOUR USER AGENT>",
+  retries : 3
 }
 const cliniko = new Cliniko(opts)
-cliniko.Settings().Public().get()
-  .then(console.log)
-
-cliniko.Settings().get()
-  .then(console.log)
-
-cliniko.User().get()
-  .then(console.log)
+cliniko.getPublicSettings().then(console.log)
+const cliniko1 = new Cliniko(opts)
+cliniko1.getSettings().then(console.log)
+const cliniko2 = new Cliniko(opts)
+cliniko2.getUser().then(console.log)
