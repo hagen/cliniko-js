6b778725a789630f449ca5296e14f575776fbcd3
Hagen Dittmer <hagen.dittmer@outlook.com>
2017-12-14 10:44:33 +1100

    Deleted a const accidentally. Didn't test. Duh

---
+++ test/deleteMedicalAlert.js
@@ -0,0 +1,7 @@
+const Cliniko = require('../').Cliniko
+const opts = require('./config')
+const cliniko = new Cliniko(opts)
+console.log("Delete medical alert")
+cliniko.deleteMedicalAlert(909348)
+  .then(console.log)
+  .catch(console.error)
