e0489a71588be2544e4fcb4563a7662480f9c197
Hagen Dittmer <hagen.dittmer@outlook.com>
2018-02-05 15:54:12 +1100

    Added practitioner reference numbers

---
+++ fields/practitioner_reference_numbers.js
@@ -0,0 +1,8 @@
+const fields = [
+  { name: 'practitioner', type: 'link', rename: 'practitioner_id' },
+  { name: 'business', type: 'link', rename: 'business_id' }
+]
+
+module.exports = {
+  fields
+}
