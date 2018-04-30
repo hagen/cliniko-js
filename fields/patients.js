cf0809a074d184f0a6940847631892a4673a6d03
Hagen Dittmer <hagen.dittmer@outlook.com>
2018-05-01 01:03:22 +1000

    Referring doctor ID in patient endpoint

--- fields/patients.js
+++ fields/patients.js
@@ -1,5 +1,6 @@
 const fields = [
-  { name: 'concession_type', type: 'link', rename: 'concession_type_id' }
+  { name: 'concession_type', type: 'link', rename: 'concession_type_id' },
+  { name: 'referring_doctor', type: 'link', rename: 'referring_doctor_id' }
 ]
 
 module.exports = {
